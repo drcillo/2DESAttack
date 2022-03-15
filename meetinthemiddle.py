47af82b9b80f60c4ab9a2bebcc7aac208ac6083f
drcillo <99564748+drcillo@users.noreply.github.com>
2022-03-15 18:10:34 -0400

    Update meetinthemiddle.py

--- meetinthemiddle.py
+++ meetinthemiddle.py
@@ -21,8 +21,8 @@ import sys
 
 # The following is your plaintext/ciphertext pair. We read it from 
 # stdin:
-plaintext = 0486DE014605796A
-ciphertext = 89504E470D0A1A0A
+plaintext = [04,86,DE,01,46,05,79,6A}
+ciphertext = [89,50,4E,47,0D,0A,1A,0A]
 
 # Read in the effective keylength in number of hex digits. This value
 # is either 3 or 4 in our case:
